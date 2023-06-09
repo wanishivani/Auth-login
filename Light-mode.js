effbd3bea85ea54d3bcbcde2941e6e482b3327d6
wanishivani <127400106+wanishivani@users.noreply.github.com>
2023-06-09 13:06:27 +0530

    Add files via upload

---
+++ Light-mode.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+// import Pages from "./Pass";
+function Light() {
+  // const [data, setData] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
+  //getting posts
+  // useEffect(() => {
+  //   const paginationFunc = async () => {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     const data = await res.json();
+  //     setData(data);
+  //     console.log(data);
+  //   };
+  //   paginationFunc();
+  // }, []);
+  return (
+    <>
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+        <div className="switch-container">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round" />
+          </label>
+          <label className="switch-label">{darkMode ? "Dark" : "Light"}</label>
+        </div>
+        {/* {data && data.length > 0 ? <Pages data={data} /> : <p>Loading...</p>} */}
+      </div>
+    </>
+  );
+}
+
+export default Light;
